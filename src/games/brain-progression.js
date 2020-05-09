4fe300985994cab69039a56866da3ffe00200c72
Andrey Solovyev <solovyev833@gmail.com>
2020-05-09 12:12:04 +0300

    Incorporate linter feedback in brain-progression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -4,6 +4,24 @@ import {
 
 const gameRules = 'Find the number missing in the progression.';
 
+const hideElement = (progression) => {
+  const modifiedProgression = [...progression];
+  const hiddenIndex = getRandom(modifiedProgression.length, 0);
+  modifiedProgression[hiddenIndex] = '..';
+
+  return modifiedProgression;
+};
+
+const evalHidden = (progression, increment) => {
+  const hiddenIndex = progression.indexOf('..');
+  if (hiddenIndex === 0) {
+    return progression[1] - increment;
+  }
+  const firstElement = progression[0];
+  const hiddenElement = firstElement + increment * hiddenIndex;
+  return hiddenElement;
+};
+
 const generateQaPair = () => {
   const progression = [getRandom()];
   const increment = getRandom(10);
@@ -18,23 +36,6 @@ const generateQaPair = () => {
   const question = hideElement(progression);
   const answer = `${evalHidden(question, increment)}`;
   return [question.join(' '), answer];
-};
-
-const hideElement = (progression) => {
-  const hiddenIndex = getRandom(progression.length, 0);
-  progression[hiddenIndex] = '..';
-
-  return progression;
-};
-
-const evalHidden = (progression, increment) => {
-  const hiddenIndex = progression.indexOf('..');
-  if (hiddenIndex === 0) {
-    return progression[1] - increment;
-  }
-  const firstElement = progression[0];
-  const hiddenElement = firstElement + increment * hiddenIndex;
-  return hiddenElement;
 };
 
 export default () => {
